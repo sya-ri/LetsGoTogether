a64eb9eb48e97dbae081adaab31588e161b63e78
sya-ri <34268371+sya-ri@users.noreply.github.com>
2021-03-25 13:38:58 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/syari/yululi/letsgotogether/Main.kt
@@ -0,0 +1,14 @@
+package com.github.syari.yululi.letsgotogether
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class Main : JavaPlugin() {
+    companion object {
+        internal lateinit var plugin: JavaPlugin
+    }
+
+    init {
+        plugin = this
+    }
+}
